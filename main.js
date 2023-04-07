5530cdf28e115ab6091d25323b2d3ea99cba2433
ahadsts9901 <ahadsts990@gmail.com>
2023-04-07 11:16:00 -0700

    updated

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function CalculateWeather(event) {
 
     let apiKey = '26ae9a0b27a97215beaa4b5df9aa15fb'
 
-    let response = ""
+    let now = moment().format('dddd MMMM Do hh:mm A')
 
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`)
         .then(function (response) {
@@ -22,10 +22,15 @@ function CalculateWeather(event) {
 
             document.querySelector('#country').innerHTML = `Country <br/> ${response.data.sys.country}`
             document.querySelector('#humidity').innerHTML = `Humidity <br/> ${response.data.main.humidity}%`
-            document.querySelector('#sky').innerHTML = `Sky <br/>${response['data']['weather']['0']['main']}`
+            document.querySelector('#sky').innerHTML = `Sky <br/> ${response['data']['weather']['0']['main']}`
             document.querySelector('#wind').innerHTML = `Wind <br/> ${(((response.data.wind.speed) * 2.9).toFixed())} km/h <br/> ${response.data.wind.deg}° <br/> From North`
             document.querySelector('#min').innerHTML = `Min Temperature <br/> ${response.data.main.temp_min}°C`
             document.querySelector('#max').innerHTML = `Max Temperature <br/> ${response.data.main.temp_max}°C`
+            document.querySelector('#pressure').innerHTML = `Pressure <br/> ${response.data.main.pressure}Pascals`
+            document.querySelector('#now').innerHTML = `${now}`
+            document.querySelector('#image').innerHTML = `<img src='https://openweathermap.org/img/wn/${response['data']['weather']['0']['icon']}@2x.png'>`
+            document.querySelector('#skytitle').innerHTML = `${response['data']['weather']['0']['main']}`
+
 
 
         })
